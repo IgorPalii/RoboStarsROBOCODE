38cf135fa96781011a1a23af04b0f1b45ff48840
Куцик Олександр <kutsik.alexander@gmail.com>
2025-05-31 21:19:23 +0300

    Lesson 31.05

--- RoboStarsProject/Assets/Scripts/MainCharacterSetting.cs
+++ RoboStarsProject/Assets/Scripts/MainCharacterSetting.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using System;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class MainCharacterSetting : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private GameNetworkManager gameManager;
     private const int MAX_HEALTH = 100;
+    private const byte GAME_OVER_CODE = 112;
 
     [SerializeField]
     private PhotonView pView;
@@ -27,11 +31,28 @@ public class MainCharacterSetting : MonoBehaviourPunCallbacks
     private void OnEnable()
     {
         OnTakeDamage += TakeDamage;
+        PhotonNetwork.NetworkingClient.EventReceived += OnNetworkEventCome;
     }
 
     private void OnDisable()
     {
         OnTakeDamage -= TakeDamage;
+        PhotonNetwork.NetworkingClient.EventReceived -= OnNetworkEventCome;
+    }
+
+    private void OnNetworkEventCome(EventData data)
+    {
+        if (data.Code == GAME_OVER_CODE)
+        {
+            if (!pView.IsMine) return;
+            gameManager.OnGameWon.Invoke();
+        }
+    }
+
+    private void SendWinEvent()
+    {
+        object[] data = null;
+        PhotonNetwork.RaiseEvent(GAME_OVER_CODE, data, RaiseEventOptions.Default, SendOptions.SendUnreliable);
     }
 
     private void TakeDamage(int value)
@@ -44,10 +65,15 @@ public class MainCharacterSetting : MonoBehaviourPunCallbacks
     {
         health -= value;
 
-        if (health <= 0) 
+        if (health <= 0)
         {
-            health = MAX_HEALTH;
-            transform.GetComponentInChildren<MainCharacter>().Respawn();
+            //health = MAX_HEALTH;
+            //transform.GetComponentInChildren<MainCharacter>().Respawn();
+            if (pView.IsMine)
+            {
+                SendWinEvent();
+                gameManager.OnGameOver.Invoke();
+            }
         }
 
         healthBar.fillAmount = health;
